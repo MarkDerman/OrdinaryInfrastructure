095d120ec09118e1aa7226d4c2a2df9b7511e001
Mark Derman <mark@soulv.co.za>
2026-02-06 08:10:27 +0200

    Starting a new assembly for manual implementations of the command\handler pattern.

---
+++ Patterns/CommandHandler/Contracts.cs
@@ -0,0 +1,35 @@
+namespace Odin.Patterns.CommandHandler;
+
+/// <summary>
+/// Defines a command request that doesn't return a value
+/// </summary>
+public interface ICommand { }
+
+/// <summary>
+///  Defines a command request that returns an operation result
+/// (e.g., a new ID, a Result class, etc.)
+/// or could be a query that returns query results data. 
+/// </summary>
+/// <typeparam name="TResult"></typeparam>
+public interface ICommand<out TResult> { }
+
+/// <summary>
+/// Defines the handling implementation for a command request that does not return a Result.
+/// </summary>
+/// <typeparam name="TCommand"></typeparam>
+public interface ICommandHandler<in TCommand> 
+    where TCommand : ICommand
+{
+    Task HandleAsync(TCommand command, CancellationToken ct = default);
+}
+
+/// <summary>
+/// Defines the handling implementation for a command request that returns a Result.
+/// </summary>
+/// <typeparam name="TCommand"></typeparam>
+/// <typeparam name="TResult"></typeparam>
+public interface ICommandHandler<in TCommand, TResult> 
+    where TCommand : ICommand<TResult>
+{
+    Task<TResult> HandleAsync(TCommand command, CancellationToken ct = default);
+}
